76eaa685fe65ad4049115ad3952cbe59e85f0d6f
Greg Russell <gfr10598@gmail.com>
2017-02-10 19:46:39 +0000

    try using deploy script

---
+++ travis/deploy_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Pushes new docker image to docker hub
+set -x
+set -e
+
+# Push the image out to dockerhub
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+export REPO=measurementlab/ndt-builder
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker tag gfr10598/mlab-builder $REPO:$COMMIT
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
+
+exit 0
