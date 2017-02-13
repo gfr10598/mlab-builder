67eaf5dba36cf6ff5b618ce82fe4ea0233f66cc9
Greg Russell <gfr10598@gmail.com>
2017-02-13 19:21:45 +0000

    Handle other forks and branches

--- travis/deploy_image.sh
+++ travis/deploy_image.sh
@@ -1,15 +1,48 @@
 #!/bin/bash
 # Pushes new docker image to docker hub
+# PREREQS: Caller (travis-ci) should have set $TARGET_IMAGE and $TRAVIS_COMMIT
+# ARGS:  Expects owner/repo and tag arguments
+
 # DO NOT set -x as that exposes passwords!
 set -e
+NAME=${1:?Please provide the full image name e.g. owner/repo}
+TAG=${2:?Please provide the commit tag, e.g. latest}
+
+owner_repo='(.*)\/(.*)'
+if [[ $NAME =~ $owner_repo ]]; then
+  OWNER=${BASH_REMATCH[1]}
+  REPO=${BASH_REMATCH[2]}
+  echo 'Owner = ' $OWNER ' repo = ' $REPO
+else
+  echo 'NAME must be in form owner/repo'
+  exit 1
+fi
+
+if [[ $TARGET_IMAGE =~ $owner_repo ]]; then
+  T_OWNER=${BASH_REMATCH[1]}
+  T_REPO=${BASH_REMATCH[2]}
+else
+  echo '$TARGET_IMAGE must be set, in owner/repo format.'
+fi
+
+echo 'Target: ' $TARGET_IMAGE
+
+if [[ $T_OWNER == $OWNER ]]; then
+  # This is the mlab origin, so just use the provided tag.
+  echo 'Adding ' $NAME':'$TAG
+  docker tag $TARGET_IMAGE':'$TRAVIS_COMMIT $NAME':'$TAG
+else 
+  # This some other fork, so adjust the name
+  echo 'Adding ' $TARGET_IMAGE':'$OWNER'-'$TAG
+  docker tag $TARGET_IMAGE':'$TRAVIS_COMMIT $TARGET_IMAGE':'$OWNER'-'$TAG
+fi
 
 # Push the image out to dockerhub
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-export REPO=measurementlab/ndt-builder
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker tag gfr10598/mlab-builder $REPO:$COMMIT
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
+echo 'Pushing ' $TARGET_IMAGE ' from ' $NAME
+docker inspect $TARGET_IMAGE':'$TRAVIS_COMMIT
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+# Push all tags
+docker push $TARGET_IMAGE
 
 exit 0
